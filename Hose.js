30287f8bd96292f73115348264340673f30becc9
p_ash <palliwar.aashay@gmail.com>
2021-02-13 10:37:30 +0530

    Implemented file-register loader

--- Hose.js
+++ Hose.js
@@ -2,9 +2,8 @@ const { jsonParser } = require("./parsers/jsonParser");
 const { yamlParser } = require("./parsers/yamlParser");
 const { envParser } = require("./parsers/envParser");
 const HoseError = require("./utils/HoseError");
-const fs = require("fs");
-const { isString, isFunction, isFile } = require("./utils/typeValidators");
-const { getFileURI } = require("./utils/getFileUri");
+const { isString, isFunction, isFile, isObject } = require("./utils/typeValidators");
+const { getFileURI, getDefaultParserTypeForFile } = require("./utils/getFileInfo");
 
 
 class Hose {
@@ -47,13 +46,74 @@ class Hose {
       throw new HoseError(`Hose Error: Definition file of unsupported format - ${fileType}`);
     }
 
-    let defUri = getFileURI(configDefinitionSource);
+    // let defUri = getFileURI(configDefinitionSource);
+    let defUri = configDefinitionSource;
+    
     if (!isFile(defUri)) {
       throw new HoseError("Hose Error: Definition file does not exist");
     }
 
-    this.fileRegister["definition"] = defUri;
     this.definition = this.defaultParsers[fileType](defUri);
+
+  }
+
+  /**
+   * Loads file register with file-parser mappings
+   */
+  loadFileMetadata() {
+
+    let files = this.definition.files;
+
+    for (const file in files) {
+      let fileUri = null;
+      let isDefault = false;
+      let parserAlias = null;
+
+      if (isString(files[file])) {
+
+        fileUri = getFileURI(files[file]);
+        isDefault = true;
+        parserAlias = getDefaultParserTypeForFile(fileUri);    
+
+      } else if (isObject(files[file])) {
+
+        let fileData = files[file];
+
+        fileUri = (fileData["isAbsolute"] === true) ? 
+          fileData["fileUri"] : getFileURI(fileData["fileUri"]);
+
+        // If a specific default parser is mentioned.
+        if (isString(fileData["useDefault"])) {
+
+          let defaultParserAvailable = this.defaultParsers
+            .hasOwnProperty(fileData["useDefault"].trim().toUpperCase());
+
+          if (defaultParserAvailable) {
+            isDefault = true;
+            parserAlias = fileData["useDefault"].trim().toUpperCase();
+          } else {
+            throw new HoseError(`Hose Error: The requested default parser for file ${file} is not available`);
+          }
+
+        } else {
+          if (isString(fileData["parserAlias"])) {
+            isDefault = false;
+            parserAlias = fileData["parserAlias"];
+          } else {
+            isDefault = true;
+            parserAlias = getDefaultParserTypeForFile(fileUri); 
+          }
+        }
+
+      }
+
+      this.fileRegister[file] = {
+        fileUri,
+        isDefault,
+        parserAlias
+      }
+
+    }
 
   }
 
@@ -99,6 +159,7 @@ class Hose {
     this.initRegistries();
     this.loadDefaultParsers();
     this.loadDefinition(configDefinitionSource, fileType);
+    this.loadFileMetadata();
 
     // this.variablesWithUnknownSource  = null // Array of variables with unknown source.
   }
---
+++ utils/getFileInfo.js
@@ -0,0 +1,38 @@
+const HoseError = require("./HoseError");
+const { isString } = require("./typeValidators");
+
+/**
+ * Gives absolute path to a file
+ * @param {String} relativePath
+ */
+let getFileURI = (relativePath) => {
+  
+  if (!isString(relativePath)) {
+    throw new HoseError("Hose Error: Relative path to a file must be a string");
+  }
+
+  let absolutePath = process.cwd() + relativePath.trim();
+
+  return absolutePath;
+
+}
+
+let getDefaultParserTypeForFile = (path) => {
+
+  if (!isString(path)) {
+    throw new HoseError("Hose Error: Path to a file must be a string");
+  }
+
+  if (path.toLowerCase().trim().endsWith(".json")) {
+    return "JSON";
+  } else if (path.toLowerCase().trim().endsWith(".yaml")) {
+    return "YAML";
+  } else {
+    return "ENV";
+  }
+}
+
+module.exports = {
+  getFileURI,
+  getDefaultParserTypeForFile
+}
